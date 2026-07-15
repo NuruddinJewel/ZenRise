c68204d49d990756477dfca58f1e6abbbba971b8
NuruddinJewel <m.76jewel@gmail.com>
2026-07-15 12:03:49 +0600

    Updated Deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,36 +1,7 @@
-// import dns from 'node:dns';
-// dns.setServers(['8.8.8.8', '8.8.4.4']);
-
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   images: {
-//     dangerouslyAllowSVG: true,
-//     contentDispositionType: 'attachment',
-//     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
-
-//     remotePatterns: [
-//       {
-//         protocol: 'https',
-//         hostname: '**',
-//         port: '',
-//         pathname: '**',
-//       },
-//       {
-//         protocol: 'http',
-//         hostname: '**',
-//         port: '',
-//         pathname: '**',
-//       },
-//     ],
-//   },
-// };
-
-// export default nextConfig;
-
-//2
-
 import dns from 'node:dns';
 dns.setServers(['8.8.8.8', '8.8.4.4']);
+
+const backendUrl = process.env.NEXT_PUBLIC_API_URL;
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -54,15 +25,15 @@ const nextConfig = {
       },
     ],
   },
+
   async rewrites() {
-    return {
-      fallback: [
-        {
-          source: '/api/:path*',
-          destination: `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`,
-        },
-      ]
-    };
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${backendUrl}/api/:path*`,
+      },
+    ];
   },
 };
 
+export default nextConfig;
